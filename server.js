ff2732e963528ccd551af307e12b8ff00e930591
bjs <127002178+benstorlie@users.noreply.github.com>
2023-06-03 22:47:39 -0700

    modified copied files to match current project

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, 'public/404.html'))
 );
 
 app.listen(PORT, () =>
